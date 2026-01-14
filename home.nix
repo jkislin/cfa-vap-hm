64d5939dc5410f535196e0820cc07023205c40ee
Giovanni Rella <giomrella@gmail.com>
2026-01-14 15:55:01 -0500

    setting EDITOR=nvim

--- home.nix
+++ home.nix
@@ -156,7 +156,7 @@
   #  /etc/profiles/per-user/gio/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "nvim";
   };
 
   # Let Home Manager install and manage itself.
